d11d046236248f1f6ee182f1514df01faf9d8807
Ishwari <pishwari23bba@student.mes.ac.in>
2026-02-09 23:56:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
 function toggleMenu() {
-  const nav = document.getElementById("nav");
-  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-}
-
-function openLightbox(src) {
-  document.getElementById("lightbox").style.display = "flex";
-  document.getElementById("lightboxImg").src = src;
-}
-
-function closeLightbox() {
-  document.getElementById("lightbox").style.display = "none";
+  document.getElementById("menu").classList.toggle("show");
 }
