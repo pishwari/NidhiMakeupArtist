0d9482cf2720679050fedc58422d8b9afb7ac4d0
Ishwari <pishwari23bba@student.mes.ac.in>
2026-02-10 20:23:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-function toggleMenu() {
-  document.getElementById("menu").classList.toggle("show");
+function toggleMenu(){
+  document.getElementById("nav").classList.toggle("show");
 }
+
+function openLightbox(text){
+  document.getElementById("lightbox").style.display="flex";
+  document.getElementById("lightboxContent").innerText=text;
+}
+
+function closeLightbox(){
+  document.getElementById("lightbox").style.display="none";
+}
+
+let slides=document.querySelectorAll(".slide");
+let index=0;
+
+setInterval(()=>{
+  slides[index].classList.remove("active");
+  index=(index+1)%slides.length;
+  slides[index].classList.add("active");
+},3000);
